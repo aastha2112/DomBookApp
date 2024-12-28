b9e1741c89d7116a30571e360959ed2b6542c1fa
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2024-12-28 16:50:23 +0530

    Added: styles to all pages

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,7 +2,7 @@ function navbar() {
   let navBar = document.getElementById("navBar");
 
   let navLinks = `<h1>DomBookApp</h1>
-        <div>
+        <div class="navlinks">
           <a href="./index.html">Home</a>
           <a href="./admin.html">Admin</a>
           <a href="./books.html">Books</a>
